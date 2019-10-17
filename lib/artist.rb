32b4d4aaba50f240c610b304e5afcc3530d649f4
Liam Jacobs <liamdavidjacobs@gmail.com>
2019-10-17 21:03:58 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -14,7 +14,8 @@ class Artist
   end
 
   def self.find_or_create_by_name(name)
-    self.all.detect { |artist| artist.name = name } 
+    artist = self.all.detect { |artist| artist.name = name }
+    
   end
 
   def add_song(song)
